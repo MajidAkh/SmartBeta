bcec3977033372c9362f47c908e67f9ad0e0d769
LEROYQuentin <70321944+LEROYQuentin@users.noreply.github.com>
2020-08-31 10:31:08 +0200

    Add files via upload

---
+++ Settings.py
@@ -0,0 +1,21 @@
+#Settings
+
+
+#Date
+start_date = "2020-01-01"
+end_date = "2020-06-30"
+
+
+#Universe
+Universe = "CACtest"
+
+
+#Number of stock you pick for each factors
+nb_Value =20
+nb_Dividends =20
+nb_Size =20
+nb_Momentum =20
+nb_LowVol =20
+
+
+
